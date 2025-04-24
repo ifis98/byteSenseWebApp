a7807f5d3fd3ddf1c369bde0d04597ac2da3c393
Yash Lad <yash@bytesense.ai>
2025-04-24 17:54:59 -0400

    https

--- exports/variable.js
+++ exports/variable.js
@@ -3,5 +3,6 @@
 // This file contains variables that are exported for use throughout the application
 // Modify the backendLink to point to your backend API
 
-export const backendLink = "http://13.59.234.168:4000/"; //real backend
+//export const backendLink = "http://13.59.234.168:4000/"; //real backend
 //export const backendLink = "http://localhost:4000/";
+export const backendLink = "https://api.bytesense.ai:4000/";
