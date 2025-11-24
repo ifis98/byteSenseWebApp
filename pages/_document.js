f0ccbaeda9115b9d0e692e249865e8144c2c4a3e
Yash Lad <36941068+ifis98@users.noreply.github.com>
2025-11-24 14:40:14 -0500

    Make Rewardful referral optional

---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html:
+                "(function(w,r){w._rwq=r;w[r]=w[r]||function(){(w[r].q=w[r].q||[]).push(arguments)}})(window,'rewardful');",
+            }}
+          />
+          <script
+            async
+            src="https://r.wdfl.co/rw.js"
+            data-rewardful="98d039"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
