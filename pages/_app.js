028abf64d856e93a4c2febfe0f01d1fa62c6b584
Yash Lad <36941068+ifis98@users.noreply.github.com>
2025-11-24 15:39:31 -0500

    Add Rewardful conversion tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 // pages/_app.js
 
 import { Provider } from 'react-redux';
+import Script from 'next/script';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from '../store/store';
 import "../styles/homepageContent.scss";
@@ -12,6 +13,17 @@ function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
+        <Script id="rewardful-loader" strategy="afterInteractive">
+          {`(function(w,r){w._rwq=r;w[r]=w[r]||function(){(w[r].q=w[r].q||[]).push(arguments)}})(window,'rewardful');`}
+        </Script>
+        {/* Next.js injects afterInteractive scripts into the document head and runs them once hydration completes. */}
+        <Script
+          id="rewardful-sdk"
+          strategy="afterInteractive"
+          async
+          src="https://r.wdfl.co/rw.js"
+          data-rewardful="98d039"
+        />
         <Component {...pageProps} />
       </PersistGate>
     </Provider>
