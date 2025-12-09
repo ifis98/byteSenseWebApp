3bba411d5005576a68fdf503469216e17d1ec577
Yash Lad <36941068+ifis98@users.noreply.github.com>
2025-12-08 21:03:35 -0500

    Comment out Staff Training navigation entry

--- components/components/homepageContent/LabSlip.js
+++ components/components/homepageContent/LabSlip.js
@@ -4,7 +4,7 @@ const LabSlip = () => {
   return (
     <div style={{ padding: "20px" }}>
       <h2 className="mb-1" style={{ color: "#ef5350", fontWeight: 600 }}>
-        Quick Start
+        Start Here
       </h2>
       <div style={{ width: "100%", height: "80vh" }}>
         <iframe
